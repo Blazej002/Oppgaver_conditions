8385cd469a0321290924985e6992c90d5a24133b
Blazej002 <141903573+Blazej002@users.noreply.github.com>
2023-11-14 14:21:06 +0100

    Add project files.

---
+++ ConsoleApp2/Ave.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ConsoleApp2
+{
+    internal class Ave
+    {
+        public static int add(int x, int y)
+        {
+            if (x == y)
+            {
+                return x * y;
+            }
+            return x + y;
+        }
+
+        public static bool checkIf(int x)
+        {
+            if (x == 30)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Principal;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Del 1 av den oppgaven ble brukt på guess number oppgaven.
+            Console.WriteLine("Hello, choose 2 numbers.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int x = Ave.add(num1, num2);
+            if (num1 == num2)
+            {
+                Console.WriteLine($"{num1} x {num2} = {x}");
+            }
+            else
+            {
+                Console.WriteLine($"{num1} + {num2} = {x}");
+            }
+
+
+            Console.WriteLine("Checking...");
+            bool trueFalse = Ave.checkIf(x);
+            if (trueFalse)
+            {
+                Console.WriteLine("The number is 30.");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine($"Number is not 30, its {x}");
+        }
+    }
+}
